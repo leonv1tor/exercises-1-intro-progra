797c5b08c9e7d0e5e87918c7fd973c002270d5a0
leonv1tor <162352872+leonv1tor@users.noreply.github.com>
2024-03-16 11:03:34 -0400

    11 deleted

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -176,25 +176,7 @@ int exercise_10(int a, int b) {
 }
 
 string exercise_11(int number) {
-  int fib_prev = 0;
-    int fib_curr = 1;
-
-    if (number == 0 || number == 1) {
-        return "Se encontro Fibonacci";
-    }
-
-    while (fib_curr <= number) {
-        if (fib_curr == number) {
-            return "Se encontro Fibonacci";
-        }
-        
-        int next = fib_prev + fib_curr;
-        fib_prev = fib_curr;
-        fib_curr = next;
-    }
-
-  return "Esto no es de Fibonacci";
-  return "";
+  //
 }
 
 void exercise_12(string color1, int numb1,
