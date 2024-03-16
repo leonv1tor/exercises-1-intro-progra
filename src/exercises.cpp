c472931f5b6411e339cbfd01e3ff814c2f10687a
leonv1tor <162352872+leonv1tor@users.noreply.github.com>
2024-03-16 01:28:39 -0400

    8.10

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -118,7 +118,20 @@ void exercise_7(double r) {
 }
 
 void exercise_8(long int seconds) {
-  //
+  void showTime(long int seconds) {
+    if (seconds < 0) {
+        throw invalid_argument("Error: Input seconds cannot be negative.");
+    }
+
+    long int hours = seconds / 3600;
+    seconds %= 3600;
+    long int minutes = seconds / 60;
+    seconds %= 60;
+
+    cout << (hours < 10 ? "0" : "") << hours << ":"
+         << (minutes < 10 ? "0" : "") << minutes << ":"
+         << (seconds < 10 ? "0" : "") << seconds << endl;
+}
 }
 
 
