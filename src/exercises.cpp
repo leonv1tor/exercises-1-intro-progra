8d8ba4e3f9ee6568e36a0a75038c23aabd127334
leonv1tor <162352872+leonv1tor@users.noreply.github.com>
2024-03-16 11:05:21 -0400

    .

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -242,7 +242,7 @@ void exercise_15(int a, int b, int c) {
     cout << "Los valores despues de la permutacion son: a = " << a << " b = " << b << " c = " << c << endl;
 
 }
-}
+
 
 void exercise_16(int debut, int fin) {
   // TODO: YOUR CODE HERE
